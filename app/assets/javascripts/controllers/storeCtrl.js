7e25265d52be1bf175da4d61da2a175aa4ffd350
Bapu L <bapu.labade@imaginea.com>
2014-06-04 12:37:03 +0530

    Change in view

--- app/assets/javascripts/controllers/storeCtrl.js
+++ app/assets/javascripts/controllers/storeCtrl.js
@@ -1,69 +1,61 @@
- 
-// var app = angular.module('gemStore', []);
-
-// app.controller('storeController', ['$scope', function($scope) {
-//    $scope.products = gems;
-// }]);
-
-
-var myApp = angular.module('gemStore',[]);
+ var myApp = angular.module('gemStore',[]);
 
   myApp.controller('storeController', ['$scope', function($scope) {
     $scope.products = gems;
   }]);
 
-  myApp.directive("productGallery", function() {
+
+  myApp.controller('productTabs', ['$scope', function($scope){
+
+    $scope.tab = 1;
+
+    $scope.isSet = function(checkTab){
+      return $scope.tab == checkTab;  
+    }
+
+    $scope.setTab = function(activeTab){
+      $scope.tab = activeTab;
+    }
+  }]);
+
+
+  myApp.controller('reviewController',['$scope', function($scope) {
+    
+    $scope.review = {};
+
+    $scope.addReview = function(product) {
+      product.reviews.push($scope.review);
+
+      $scope.review = {};
+    };
+
+  }]);
+
+
+  myApp.directive("productDescription", function() {
     return {
-      restrict: "E",
-      templateUrl: "assets/templates/product-gallery.html",
-      controller: function() {
-        this.current = 0;
-        this.setCurrent = function(imageNumber){
-          this.current = imageNumber || 0;
-        };
-      },
-      controllerAs: "gallery"
+      restrict: 'A',
+      templateUrl: "assets/product-descriptions.html"
     };
   });
 
-// angular.module('gemStore', [])
-//   .controller('StoreController', ['$scope', function($scope) {
-//     $scope.products = gems;
-//   }]);
+
+  myApp.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "assets/product-reviews.html"
+    };
+  });
+
+  myApp.directive("productSpecs", function() {
+    return {
+      restrict:"A",
+      templateUrl: "assets/product-specs.html"
+    };
+  });
 
 
-  // .directive('myCustomer', function() {
-  //   return {
-  //     templateUrl: 'my-customer.html'
-  //   };
-  // });
-
-
-//   function storeController($scope) {
-
-// 	// function findPost(id){
-// 	// 	$http({method: 'GET', url: './posts/'+id+'.json'}).
-//  //      success(function(data, status, headers, config) {
-//  //      // this callback will be called asynchronously
-//  //      // when the response is available
-//  //      $scope.post = data;
-      
-//  //    }).error(function(data, status, headers, config) {
-//  //      // called asynchronously if an error occurs
-//  //      // or server returns response with an error status.
-//  //      console.log("Error while loading posts...");
-//  //    });
-// 	// }
-
-//   // Make Ajax call to find post based on post id
-
-// 	// findPost($routeParams.id);
-
-// 	 $scope.products = gems;
-// };
-
-
-var gems = [{
+  var gems = [{
       name: 'Azurite',
       description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
       shine: 8,
@@ -72,9 +64,9 @@ var gems = [{
       color: '#CCC',
       faces: 14,
       images: [
-        "images/gem_01.jpg",
-        "images/gem_02.jpg",
-        "images/gem_03.jpg"
+        "assets/gem_01.jpg",
+        "assets/gem_02.jpg",
+        "assets/gem_03.jpg"
       ],
       reviews: []
     }, {
@@ -86,9 +78,9 @@ var gems = [{
       color: '#EEE',
       faces: 12,
       images: [
-        "images/gem_03.jpg",
-        "images/gem_05.jpg",
-        "images/gem_06.jpg",
+        "assets/gem_03.jpg",
+        "assets/gem_05.jpg",
+        "assets/gem_06.jpg",
       ],
       reviews: []
     }, {
@@ -100,9 +92,9 @@ var gems = [{
       color: '#000',
       faces: 6,
       images: [
-        "images/gem_01.jpg",
-        "images/gem_02.jpg",
-        "images/gem_03.jpg"
+        "assets/gem_01.jpg",
+        "assets/gem_02.jpg",
+        "assets/gem_03.jpg"
       ],
       reviews: []
     }];
